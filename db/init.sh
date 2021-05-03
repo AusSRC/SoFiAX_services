5e2834ea8653d3a6b72c3e3e86c36a6b8ded0017
Austin Shen <austin.shen@csiro.au>
2021-05-03 14:40:52 +0800

    dockerfile initialising database

--- db/init.sh
+++ db/init.sh
@@ -2,7 +2,7 @@
 
 # Create database and users from empty PostgreSQL server locally
 createdb sofiadb
-psql -h localhost -f create.sql
-psql -h localhost -f db.sql
-psql -h localhost -f update.sql
-psql -h localhost -f privileges.sql
+psql -f create.sql
+psql -f db.sql
+psql -f update.sql
+psql -f privileges.sql
