b048f96d1a33f45879384140627086b3671acc8b
Austin Shen <austin.shen@csiro.au>
2021-09-08 09:39:21 +0800

    removing database scripts here to separate repository https://github.com/AusSRC/WALLABY_database

--- db/init.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create database and users from empty PostgreSQL server locally
-createdb sofiadb
-psql -h localhost -f create.sql
-psql -h localhost -f db.sql
-psql -h localhost -f update.sql
-psql -h localhost -f privileges.sql
