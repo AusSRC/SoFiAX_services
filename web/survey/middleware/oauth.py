291997bc621ef01a026558eb2695fecbb8471126
davepallot <dave.pallot@uwa.edu.au>
2023-11-20 07:48:04 +0000

    Only allow specific groups to access portal

--- web/survey/middleware/oauth.py
+++ web/survey/middleware/oauth.py
@@ -1,17 +1,17 @@
+import json
+
 from django.conf import settings
 from django.contrib.auth import logout
 from django.contrib.auth.models import Group
 from django.shortcuts import redirect
 from keycloak import KeycloakOpenID
-
-import json
-
+from django.http import HttpResponse
 
 class KeycloakMiddleware:
 
     def __init__(self, get_response):
         self.get_response = get_response
-
+        self.groups = settings.AUTH_GROUPS
         self.openid = KeycloakOpenID(server_url=settings.CLIENT_AUTH,
                                      client_id=settings.SOCIAL_AUTH_KEYCLOAK_KEY,
                                      realm_name=settings.REALM,
@@ -30,10 +30,19 @@ class KeycloakMiddleware:
             if auth is None:
                 response = self.get_response(request)
                 return response
+            
             extra = auth.extra_data
             if isinstance(extra, str):
                 extra = json.loads(extra)
+            # validate access token
             data = self.openid.introspect(extra.get('access_token'))
+            # check user is a member of group that can access site
+            jwt_groups = data.get('user_groups', None)
+            if jwt_groups:
+                if any(item in self.groups for item in jwt_groups) is False:
+                    err = f"Unauthorized, not a member of any group: {', '.join(self.groups)}"
+                    return HttpResponse(err, status=401)
+
             if data.get('active', False) is False:
                 logout(request)
                 return redirect(settings.LOGIN_REDIRECT_URL)
