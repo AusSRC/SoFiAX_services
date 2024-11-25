223a06c73cf162eb578d037489a365f3a451eeaa
davepallot <dave.pallot@uwa.edu.au>
2024-11-25 08:36:10 +0000

    pushing latest changes

---
+++ vo_tmp/startup.sh
@@ -0,0 +1,10 @@
+gavo imp //dc_tables
+gavo imp //tap
+gavo imp //services
+gavo imp //users
+gavo imp //adql
+gavo imp //products
+gavo imp //uws
+gavo imp -m survey/vo.rd
+gavo pub survey/vo.rd
+gavo serve start -f
---
+++ web/run_local.sh
@@ -0,0 +1,1 @@
+python manage.py runsslserver 0.0.0.0:8443 --certificate /home/ubuntu/aussrc.org.pem --key /home/ubuntu/aussrc.org.key
