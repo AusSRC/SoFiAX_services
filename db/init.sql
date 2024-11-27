3cbed1d5e0ac38faca42e8381d9deb56ac9b744d
Tushar <tushar.patel@aussrc.org>
2024-11-27 12:17:06 +0000

    dingo - prepare for migration

---
+++ db/init.sql
@@ -0,0 +1,55 @@
+-- Ensure dblink extension is available
+CREATE EXTENSION IF NOT EXISTS dblink;
+
+-- Create database if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_database WHERE datname = 'dingo_db') THEN
+        PERFORM dblink_exec('dbname=postgres', 'CREATE DATABASE dingo_db WITH TEMPLATE = template0 ENCODING = ''UTF8'' LC_COLLATE = ''en_US.utf8'' LC_CTYPE = ''en_US.utf8''');
+    END IF;
+END
+$$;
+
+-- Create admin user if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'admin') THEN
+        CREATE USER "admin";
+        ALTER USER "admin" WITH PASSWORD 'admin';
+        ALTER USER "admin" WITH SUPERUSER;
+    END IF;
+END
+$$;
+
+-- Create wallaby user if it does not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'survey_user') THEN
+        CREATE USER "survey_user";
+        ALTER USER "survey_user" WITH PASSWORD 'survey_user';
+    END IF;
+END
+$$;
+
+-- Create VO users if they do not exist
+DO $$
+BEGIN
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'gavo') THEN
+        CREATE USER "gavo";
+        ALTER USER "gavo" WITH PASSWORD 'gavo';
+    END IF;
+
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'gavoadmin') THEN
+        CREATE USER "gavoadmin";
+        ALTER USER "gavoadmin" WITH PASSWORD 'gavoadmin';
+    END IF;
+
+    IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = 'untrusted') THEN
+        CREATE USER "untrusted";
+        ALTER USER "untrusted" WITH PASSWORD 'untrusted';
+    END IF;
+END
+$$;
+
+-- Set the owner of the wallaby database to admin
+ALTER DATABASE dingo_db OWNER TO "admin";
