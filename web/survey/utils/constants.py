b54698e3b69d93403bbb1a8ee91d2e79472f93b4
riniangreani <rini.angreani@curtin.edu.au>
2025-07-09 11:12:00 +0800

    Fix wrong name pattern for quality check

--- web/survey/utils/constants.py
+++ web/survey/utils/constants.py
@@ -1,2 +1,2 @@
 """Constants for survey run naming patterns and matching survey components."""
-RUN_NAME_TO_SURVEY_COMPONENT = {r"^SB.*_gc$": "Quality-Check", r"^SER.*$": "WALLABY v1"}
+RUN_NAME_TO_SURVEY_COMPONENT = {r"^SB.*_qc$": "Quality-Check", r"^SER.*$": "WALLABY v1"}
